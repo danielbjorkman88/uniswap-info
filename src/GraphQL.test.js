3fe8d96f0d74768bc27d2e59e66466ee96387caf
Daniel <danielbjorkman88@outlook.com>
2021-05-13 14:41:52 +0200

    added test suit for GraphQL

---
+++ src/GraphQL.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as client from './apollo/client';
+import * as queries from './apollo/queries';
+
+
+it('Test goes here', () => {
+  //const div = document.createElement('div');
+  ////ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
+});
